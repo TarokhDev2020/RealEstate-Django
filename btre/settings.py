f9d3ffa673578b67553b42b2859b4267fabf3f6f
Tarokh Mohammadi <tarokhDevMoh2020@gmail.com>
2022-02-09 18:47:29 +0330

    some minor changes

--- btre/settings.py
+++ btre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secrets import secret_key as secret_key
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ju0=kkbb%q-!5a34z8k!a!%)=wn)&(&jx1h9k=^8)ba2h$jg3j'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
